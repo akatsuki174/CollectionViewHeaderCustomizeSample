77a87e62e6761749b2ef74dd6e4c58ba1f71d36a
akatsuki174 <dev2015js@gmail.com>
2017-11-11 20:50:58 +0900

    適当にcellを表示

--- CollectionViewHeaderCustomizeSample/ViewController.swift
+++ CollectionViewHeaderCustomizeSample/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UICollectionViewController {
+final class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,3 +14,21 @@ class ViewController: UICollectionViewController {
 
 }
 
+// MARK: datasorce, delegate
+
+extension ViewController {
+    override func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+}
